b41c3178d17df887a11499dc8301bb75cf197914
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-06-26 18:55:32 +0900

    remove css from browserfy demo

--- browserfy-demo/src/App.vue
+++ browserfy-demo/src/App.vue
@@ -30,32 +30,3 @@ const devClient = createClient("devnet");
 const client1Store = client.jobs.getProgramNetwork();
 const client2Store = devClient.jobs.getProgramNetwork();
 </script>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
